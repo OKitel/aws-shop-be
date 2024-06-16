9374ee316c7e0eec67420457fe4024d61efa7d6e
OKitel <kitelolga@gmail.com>
2024-06-16 23:40:39 +0200

    fix: update path to images

--- product_service/product_service/lambda_func/product_by_id.py
+++ product_service/product_service/lambda_func/product_by_id.py
@@ -1,14 +1,16 @@
 import json
 
+BASE_URL = "https://d266s2h0r1qt2p.cloudfront.net/assets/img/"
+
 def handler(event, context):
   products = [
-    {"id": "1", "name": "Terraforming Mars", "price": 35, "img": "../assets/terraforming-mars.jpg"},
-    {"id": "2", "name": "Wingspan", "price": 40, "img": "../assets/wingspan.jpg"},
-    {"id": "3", "name": "Everdell", "price": 45, "img": "../assets/everdell.jpg"},
-    {"id": "4", "name": "Ticket to Ride", "price": 47, "img": "../assets/ticket-to-ride.jpg"},
-    {"id": "5", "name": "Carcassonne", "price": 25, "img": "../assets/carcassonne.jpg"},
-    {"id": "6", "name": "7 Wonders Duel", "price": 20, "img": "../assets/7-wonders-duel.jpg"},
-    {"id": "7", "name": "Dune: Imperium", "price": 40, "img": "../assets/dune-imperium.jpg"},
+    {"id": "1", "name": "Terraforming Mars", "price": 35, "img": f"{BASE_URL}terraforming-mars.jpg"},
+    {"id": "2", "name": "Wingspan", "price": 40, "img": f"{BASE_URL}wingspan.jpg"},
+    {"id": "3", "name": "Everdell", "price": 45, "img": f"{BASE_URL}everdell.jpg"},
+    {"id": "4", "name": "Ticket to Ride", "price": 47, "img": f"{BASE_URL}ticket-to-ride.jpg"},
+    {"id": "5", "name": "Carcassonne", "price": 25, "img": f"{BASE_URL}carcassonne.jpg"},
+    {"id": "6", "name": "7 Wonders Duel", "price": 20, "img": f"{BASE_URL}7-wonders-duel.jpg"},
+    {"id": "7", "name": "Dune: Imperium", "price": 40, "img": f"{BASE_URL}dune-imperium.jpg"},
   ]
   product_id = event["pathParameters"]["productId"]
   product = next((p for p in products if p["id"] == product_id), None)
