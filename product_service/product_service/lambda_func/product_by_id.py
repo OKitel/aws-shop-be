57bddcd9bb4718129096e3bb9faba02db936fada
OKitel <kitelolga@gmail.com>
2024-06-23 20:04:58 +0200

    feat: get products data from DynamoDB

--- product_service/product_service/lambda_func/product_by_id.py
+++ product_service/product_service/lambda_func/product_by_id.py
@@ -1,36 +1,61 @@
 import json
+import os
+import boto3
 
 BASE_URL = "https://d266s2h0r1qt2p.cloudfront.net/assets/img/"
 
 def handler(event, context):
-  products = [
-    {"id": "1", "title": "Terraforming Mars", "price": 35, "img": f"{BASE_URL}terraforming-mars.jpg"},
-    {"id": "2", "title": "Wingspan", "price": 40, "img": f"{BASE_URL}wingspan.jpg"},
-    {"id": "3", "title": "Everdell", "price": 45, "img": f"{BASE_URL}everdell.jpg"},
-    {"id": "4", "title": "Ticket to Ride", "price": 47, "img": f"{BASE_URL}ticket-to-ride.jpg"},
-    {"id": "5", "title": "Carcassonne", "price": 25, "img": f"{BASE_URL}carcassonne.jpg"},
-    {"id": "6", "title": "7 Wonders Duel", "price": 20, "img": f"{BASE_URL}7-wonders-duel.jpg"},
-    {"id": "7", "title": "Dune: Imperium", "price": 40, "img": f"{BASE_URL}dune-imperium.jpg"},
-  ]
-  product_id = event["pathParameters"]["productId"]
-  product = next((p for p in products if p["id"] == product_id), None)
-  if product:
+  
+  try:
+    product_id = event["pathParameters"]["productId"]
+    dynamodb = boto3.resource('dynamodb', region_name=os.getenv('AWS_REGION'))
+
+    products_table_name = os.getenv('PRODUCTS_TABLE_NAME')
+    stocks_table_name = os.getenv('STOCKS_TABLE_NAME')
+
+    products_table = dynamodb.Table(products_table_name)
+    stocks_table = dynamodb.Table(stocks_table_name)
+
+    product_item = products_table.get_item(Key={'id': product_id})
+    stock_item = stocks_table.get_item(Key={'product_id': product_id})
+
+    if 'Item' in product_item and 'Item' in stock_item:
+      product = {
+        'id': product_item['Item']['id'],
+        'title': product_item['Item']['title'],
+        'description': product_item['Item']['description'],
+        'price': str(product_item['Item']['price']),
+        'img': BASE_URL + product_item['Item']['img'],
+        'count': str(stock_item['Item']['count']),
+      }
+
+      return {
+      "statusCode": 200,
+      "headers": {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "content-type": "application/json"
+      },
+      "body": json.dumps(product)
+    }
+    else:
+      return {
+      "statusCode": 404,
+      "headers": {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "content-type": "application/json"
+      },
+      "body": json.dumps("'message': 'Product not found'")
+    }
+
+  except Exception as e:
     return {
-    "statusCode": 200,
-    "headers": {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "content-type": "application/json"
-    },
-    "body": json.dumps(product)
-  }
-  else:
-    return {
-    "statusCode": 404,
-    "headers": {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "content-type": "application/json"
-    },
-    "body": json.dumps("'message': 'Product not found'")
-  }
+      'statusCode': 500,
+      "headers": {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "content-type": "application/json"
+      },
+      'body': json.dumps({'error': str(e)})
+    }
