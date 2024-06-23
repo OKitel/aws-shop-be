337301cdfe50e3b71785bd38002906bab608a444
OKitel <kitelolga@gmail.com>
2024-06-23 20:22:23 +0200

    feat: add put_product lambda function to create product in db

--- product_service/product_service/api_gateway.py
+++ product_service/product_service/api_gateway.py
@@ -8,13 +8,15 @@ from constructs import Construct
 
 class ApiGateway(Stack):
 
-  def __init__(self, scope: Construct, construct_id: str, get_products_list_fn: _lambda, get_product_by_id_fn: _lambda, **kwargs) -> None:
+  def __init__(self, scope: Construct, construct_id: str, get_products_list_fn: _lambda, get_product_by_id_fn: _lambda, put_products_fn: _lambda,**kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
     api = apigateway.RestApi(self, "ProductServiceApi", rest_api_name="Product Service")
 
     products_resource = api.root.add_resource("products")
     products_resource.add_method("GET", apigateway.LambdaIntegration(get_products_list_fn))
+    products_resource.add_method("POST", apigateway.LambdaIntegration(put_products_fn))
 
     product_by_id_resource = products_resource.add_resource("{productId}")
     product_by_id_resource.add_method("GET", apigateway.LambdaIntegration(get_product_by_id_fn))
+
