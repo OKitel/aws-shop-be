02ed1ec9fc981ff52e09b7e9962ffd3547450aa8
OKitel <kitelolga@gmail.com>
2024-07-12 15:22:28 +0200

    feat:  add authorizer configuration for import_service importProductsFile lambda

--- import_service/import_service/api_gateway.py
+++ import_service/import_service/api_gateway.py
@@ -10,7 +10,15 @@ class ApiGateway(Stack):
   def __init__(self, scope: Construct, construct_id: str, import_products_fn: _lambda, **kwargs) -> None:
     super().__init__(scope, construct_id, **kwargs)
 
+    basic_authorizer_lambda = _lambda.Function.from_function_name(self, "authFunction", "AuthFunction")
+
     api = apigateway.RestApi(self, "importApi", rest_api_name="Import Service")
 
+    authorizer = apigateway.TokenAuthorizer(
+      self, 'BasicAuthorizer',
+      handler=basic_authorizer_lambda,
+      identity_source='method.request.header.Authorization'
+    )
+
     import_resource = api.root.add_resource("import")
-    import_resource.add_method("GET", apigateway.LambdaIntegration(import_products_fn))
+    import_resource.add_method("GET", apigateway.LambdaIntegration(import_products_fn), authorization_type=apigateway.AuthorizationType.CUSTOM, authorizer=authorizer)
